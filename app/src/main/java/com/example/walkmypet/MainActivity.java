b0c221a2bd1e30bcb29dba9c8fc6904bd1dea704
camilagarcia36 <garciiaperezz@gmail.com>
2022-04-20 22:52:08 +0200

    navegacion registro

--- app/src/main/java/com/example/walkmypet/MainActivity.java
+++ app/src/main/java/com/example/walkmypet/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.walkmypet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button buttonRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main) ;
+
+        buttonRegister = findViewById(R.id.comenzarRegistroButton);
+
     }
+
+    public void goToRegister(View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
