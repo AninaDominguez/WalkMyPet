2ef76498bebaf3ccafb400209c4121a45e3aa38d
camilagarcia36 <garciiaperezz@gmail.com>
2022-04-21 21:15:24 +0200

    vuelta commit prueba

--- app/src/main/java/com/example/walkmypet/MainActivity.java
+++ app/src/main/java/com/example/walkmypet/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     public void goToRegister(View view){
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
-        
     }
 
 
