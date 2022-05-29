da61a338d6f0da63fef0054fd69abfc6a0d1adb6
Saul-t16 <saul_gonzalo_tenesaca_tsapp1oa2021@cev.com>
2022-05-29 20:52:45 +0200

    intent

--- app/src/main/java/com/example/walkmypet/LocalizacionActivity.java
+++ app/src/main/java/com/example/walkmypet/LocalizacionActivity.java
@@ -3,14 +3,26 @@ package com.example.walkmypet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LocalizacionActivity extends AppCompatActivity {
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localizacion);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
 
