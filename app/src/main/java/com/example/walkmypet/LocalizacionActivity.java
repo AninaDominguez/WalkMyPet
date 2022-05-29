44f9c5132f7176130b999dee7bf9e75f1ee099d8
Saul-t16 <saul_gonzalo_tenesaca_tsapp1oa2021@cev.com>
2022-05-29 21:36:30 +0200

    ubicacion message

--- app/src/main/java/com/example/walkmypet/LocalizacionActivity.java
+++ app/src/main/java/com/example/walkmypet/LocalizacionActivity.java
@@ -1,33 +1,32 @@
 package com.example.walkmypet;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LocalizacionActivity extends AppCompatActivity {
     Button btn1, btn2;
+    Boolean pasar=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localizacion);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Ubicación");
+        builder.setMessage("¿Permites que esta app active tu ubicación?");
 
-            }
-        });
-
-    }
-    public void confirmarLocalizacion(View view){
+        builder.setPositiveButton("Aceptar", null);
+        builder.setNegativeButton("Cancelar", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
-
-
 }
