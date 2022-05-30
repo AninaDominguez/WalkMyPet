1ace32d02ffa10c56914f7ec68e65148305df2a9
Admin <garciiaperezz@gmail.com>
2022-05-30 20:55:15 +0200

    Cambios propietario botones

--- app/src/main/java/com/example/walkmypet/Cuidador.java
+++ app/src/main/java/com/example/walkmypet/Cuidador.java
@@ -1,5 +1,9 @@
 package com.example.walkmypet;
 
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.RatingBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -8,70 +12,12 @@ import java.util.List;
 
 public class Cuidador extends AppCompatActivity {
 
-    //Clase cuidador, con los atributos necesarios para el constructor, tambien el array de fechas
-    String name;
-    String surname;
-    String alias;
-    String email;
-    Float rating;
 
-    //Generados Getters y Setters
-    public String getName() {
-        return name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_walker);
+
+
     }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Float getRating() {
-        return rating;
-    }
-
-    public void setRating(Float rating) {
-        this.rating = rating;
-    }
-
-    public int getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(int photo) {
-        this.photo = photo;
-    }
-
-    public List<Calendar> getAvailableDates() {
-        return availableDates;
-    }
-
-    public void setAvailableDates(List<Calendar> availableDates) {
-        this.availableDates = availableDates;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    int photo;
-    List<Calendar> availableDates = new ArrayList<>();
 }
